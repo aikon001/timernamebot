52c1cb68c68a194124c68e3333834d402dd7979f
aikon001 <46329166+aikon001@users.noreply.github.com>
2020-03-07 22:38:03 +0100

    Add files via upload

--- bot.py
+++ bot.py
@@ -107,6 +107,9 @@ def main():
     # Start the Bot
     updater.start_polling()
 
+    port = os.getenv('PORT', default=8000)
+    updater.start_webhook(port=port)
+    
     # Block until you press Ctrl-C or the process receives SIGINT, SIGTERM or
     # SIGABRT. This should be used most of the time, since start_polling() is
     # non-blocking and will stop the bot gracefully.
