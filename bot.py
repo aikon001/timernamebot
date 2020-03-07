403eadf443bc963ca1803c825c89b0c554bfdb3b
aikon001 <46329166+aikon001@users.noreply.github.com>
2020-03-07 23:17:14 +0100

    Add files via upload

--- bot.py
+++ bot.py
@@ -105,6 +105,23 @@ def main():
     dp.add_error_handler(error)
 
     # Start the Bot
+    
+    const Telegraf = require('telegraf')
+    const express = require('express')
+    const expressApp = express()
+
+    const port = process.env.PORT || 3000
+    expressApp.get('/', (req, res) => {
+      res.send('Hello World!')
+    })
+    expressApp.listen(port, () => {
+      console.log(`Listening on port ${port}`)
+    })
+
+    const bot = new Telegraf(process.env.BOT_TOKEN)
+    bot.hears(/./, (ctx) => ctx.reply('Hello'))
+    bot.startPolling()
+    
     updater.start_polling()
 
     # Block until you press Ctrl-C or the process receives SIGINT, SIGTERM or
