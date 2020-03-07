79393528589cc6c433a104609635a2805695809e
aikon001 <46329166+aikon001@users.noreply.github.com>
2020-03-07 23:14:41 +0100

    Add files via upload

--- bot.py
+++ bot.py
@@ -42,27 +42,26 @@ def alarm(context):
 
 
 def set_timer(update, context):
-  while true:
-      """Add a job to the queue."""
-      chat_id = update.message.chat_id
-      try:
-          # args[0] should contain the time for the timer in seconds
-          due = int(context.args[0])
-          if due < 0:
-              update.message.reply_text('Sorry we can not go back to future!')
-              return
+    """Add a job to the queue."""
+    chat_id = update.message.chat_id
+    try:
+        # args[0] should contain the time for the timer in seconds
+        due = int(context.args[0])
+        if due < 0:
+            update.message.reply_text('Sorry we can not go back to future!')
+            return
 
-          # Add job to queue and stop current one if there is a timer already
-          if 'job' in context.chat_data:
-              old_job = context.chat_data['job']
-              old_job.schedule_removal()
-          new_job = context.job_queue.run_once(alarm, due, context=chat_id)
-          context.chat_data['job'] = new_job
+        # Add job to queue and stop current one if there is a timer already
+        if 'job' in context.chat_data:
+            old_job = context.chat_data['job']
+            old_job.schedule_removal()
+        new_job = context.job_queue.run_once(alarm, due, context=chat_id)
+        context.chat_data['job'] = new_job
 
-          update.message.reply_text('Timer successfully set!')
+        update.message.reply_text('Timer successfully set!')
 
-      except (IndexError, ValueError):
-          update.message.reply_text('Usage: /set <seconds>')
+    except (IndexError, ValueError):
+        update.message.reply_text('Usage: /set <seconds>')
 
 
 def unset(update, context):
